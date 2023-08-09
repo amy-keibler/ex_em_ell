3e16eea1b46b8d385b1536ab642a6e0002e7c5d0
Amelia Keibler <amelia.keibler@gmail.com>
2023-08-08 22:05:53 -0400

    Add initial ToXmlElement

--- ex_em_ell/tests/simple.rs
+++ ex_em_ell/tests/simple.rs
@@ -1,13 +1,22 @@
 #[derive(Debug, PartialEq, ex_em_ell::ToXmlDocument)]
 struct Example {
     field: String,
+    child: ExampleChild,
+}
+
+#[derive(Debug, PartialEq, ex_em_ell::ToXmlElement)]
+struct ExampleChild {
+    field: String,
 }
 
 #[test]
 fn test_from_xml() {
-    let input = "<example><field>value</field></example>";
+    let input = "<example><field>value</field><child><field>value</field></child></example>";
     let expected = Example {
         field: "value".to_string(),
+        child: ExampleChild {
+            field: "value".to_string(),
+        },
     };
 }
 
@@ -15,6 +24,9 @@ fn test_from_xml() {
 fn test_to_xml() {
     let input = Example {
         field: "value".to_string(),
+        child: ExampleChild {
+            field: "value".to_string(),
+        },
     };
     let actual = ex_em_ell::to_string_pretty(&input).expect("Failed to output XML");
     insta::assert_snapshot!(actual);
--- ex_em_ell_derive/src/lib.rs
+++ ex_em_ell_derive/src/lib.rs
@@ -40,8 +40,6 @@ pub fn enecode_derive_document(input: proc_macro::TokenStream) -> proc_macro::To
         }
     };
 
-    eprintln!("{}", expanded);
-
     // Hand the output tokens back to the compiler.
     proc_macro::TokenStream::from(expanded)
 }
@@ -49,7 +47,29 @@ pub fn enecode_derive_document(input: proc_macro::TokenStream) -> proc_macro::To
 #[proc_macro_error::proc_macro_error]
 #[proc_macro_derive(ToXmlElement, attributes(ex_em_ell))]
 pub fn enecode_derive_element(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    todo!()
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let write_xml_element = generate_write_xml_element(&input);
+
+    let name = input.ident;
+
+    // Add a bound `T: FromXmlElement` to every type parameter T.
+    let generics = add_to_xml_element_trait_bounds(input.generics);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let expanded = quote! {
+        // The generated impl.
+        impl #impl_generics ex_em_ell::traits::ToXmlElement for #name #ty_generics #where_clause {
+            fn to_xml_element<W: std::io::Write>(self: &Self, writer: &mut ex_em_ell::xml::EventWriter<W>, tag: &str) -> Result<(), ex_em_ell::errors::XmlWriteError>
+            {
+                #write_xml_element
+
+                Ok(())
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler.
+    proc_macro::TokenStream::from(expanded)
 }
 
 // Add a bound `T: ToXmlElement` to every type parameter T.
@@ -98,6 +118,43 @@ fn generate_write_xml_document(input: &DeriveInput) -> TokenStream {
         #field_writers
 
         writer.write(ex_em_ell::xml::writer::XmlEvent::end_element()).map_err(ex_em_ell::xml_utils::to_xml_write_error(#tag_name))?;
+
+    }
+}
+
+// TODO: pass in an identifier rather than assuming one (and then simplify this with generate_write_xml_document, with the ability to handle namespaces)
+fn generate_write_xml_element(input: &DeriveInput) -> TokenStream {
+    let field_writers: TokenStream = match input.data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f
+                        .ident
+                        .as_ref()
+                        .expect("Named field should have an identifier");
+                    let field_tag_name = name.to_string().to_lower_camel_case();
+
+                    quote_spanned! { f.span() =>
+                       ex_em_ell::traits::ToXmlElement::to_xml_element(&self.#name, writer, #field_tag_name)?;
+                    }
+                });
+                quote! {
+                    #(#recurse)*
+                }
+            }
+            Fields::Unnamed(ref fields) => unimplemented!(),
+            Fields::Unit => unimplemented!(),
+        },
+        Data::Enum(_) => unimplemented!(),
+        Data::Union(_) => unimplemented!(),
+    };
+
+    quote! {
+        writer.write(ex_em_ell::xml::writer::XmlEvent::start_element(tag)).map_err(ex_em_ell::xml_utils::to_xml_write_error(tag))?;
+
+        #field_writers
+
+        writer.write(ex_em_ell::xml::writer::XmlEvent::end_element()).map_err(ex_em_ell::xml_utils::to_xml_write_error(tag))?;
 
     }
 }
